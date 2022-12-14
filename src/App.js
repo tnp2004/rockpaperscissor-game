15780705053c483ef2eaaf62db32612c56881e25
tnp2004 <101316716+tnp2004@users.noreply.github.com>
2022-12-14 22:00:24 +0700

    Updates game logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
 import GameButton from './Components/GameButton';
 import GameDisplay from './Components/GameDisplay';
+import { useState } from 'react';
 
 function App() {
+  const [battleData, setBattleData] = useState([])
+  const formData = {
+    Player:'',
+    Bot:'',
+    Result:''
+  }
+
   const Selected = (answer) => {
-    alert(answer)
+    gameLogic(answer)
+  }
+
+  const gameLogic = (answer) => {
+    switch(answer) {
+      case "ROCK":
+        return Rock(botAnswer())
+      case "PAPER":
+        return Paper(botAnswer())
+      case "SCISSOR":
+        return Scissor(botAnswer())
+    }
+  }
+
+  const Rock = (answer) => {
+    formData.Player = "ROCK"
+    formData.Bot = answer
+    if (answer === "PAPER") {
+      return Winner(false)
+    }else if (answer === "SCISSOR") {
+      return Winner(true)
+    }else {
+      return Winner(null)
+    }
+  }
+  const Paper = (answer) => {
+    formData.Player = "PAPER"
+    formData.Bot = answer
+    if (answer === "SCISSOR") {
+      return Winner(false)
+    }else if (answer === "ROCK") {
+      return Winner(true)
+    }else {
+      return Winner(null)
+    }
+  }
+  const Scissor= (answer) => {
+    formData.Player = "SCISSOR"
+    formData.Bot = answer
+    if (answer === "ROCK") {
+      return Winner(false)
+    }else if (answer === "PAPER") {
+      return Winner(true)
+    }else {
+      return Winner(null)
+    }
+  }
+
+  const Winner = (result) => {
+    if (result !== null) {
+      if (result) {
+        formData.Result = "WIN"
+      }
+      else if (!result) {
+        formData.Result = "LOSE"
+      }
+    }else formData.Result = "DRAW"
+    
+    setBattleData((prevData) => {
+      return [formData, ...prevData]
+     })
+  }
+
+  const answerArray = ["ROCK", "PAPER", "SCISSOR"]
+  const botAnswer = () => {
+    const random = Math.floor(Math.random() * answerArray.length)
+    return answerArray[random]
   }
 
   return (
@@ -13,7 +86,7 @@ function App() {
       <div className='my-20 mx-auto text-center'>
         <h1 className='text-5xl'>RockPaperScissor</h1>
       </div>
-      <GameDisplay/>
+      <GameDisplay battleData={battleData} />
       <GameButton getAnswer={Selected} />
     </div>
   );
--- src/Components/GameDisplay.js
+++ src/Components/GameDisplay.js
@@ -1,7 +1,9 @@
-const GameDisplay = () => {
+const GameDisplay = (props) => {
+    const { battleData } = props
+    console.log("=>", battleData)
     return (
         <div className="border-2 border-solid mx-auto w-4/6 h-96 my-20">
-            game display
+            123
         </div>
     )
 }
